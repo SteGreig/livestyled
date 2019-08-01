3b4eec69f08e9836ea7ec5e370c1c23146e816a0
Stephen Greig <stephengreig@Stephens-MacBook-Pro.local>
2019-08-01 22:48:44 +0100

    Styled GDPR notice

--- wp-content/themes/livestyled/templates/global-gdpr.php
+++ wp-content/themes/livestyled/templates/global-gdpr.php
@@ -7,13 +7,11 @@
 	$gdprContent = $gdpr['intro_copy'];
 
 ?>
-<article class="section section--gdpr">
+<article class="section section--gdpr flexbox">
 		
-	<section class="section--gdpr-outer">
+	<section class="section--gdpr-outer flexbox justify-content-center">
 	
-		<div class="section--gdpr-inner">
-
-		<img src="https://via.placeholder.com/250x60?text=Company+Logo">
+		<div class="section--gdpr-inner flexbox flex-wrap align-items-center justify-content-center">
 				
 			<?php if( $gdprHeader ): ?>
 			<h1 class="gdpr__header"><?php echo $gdprHeader; ?></h1>
@@ -22,8 +20,10 @@
 			<?php if( $gdprContent ): echo $gdprContent; endif;?>
 
 			<?php if( $gdprAcceptText ): ?>
-			<button class="btn btn--accept gtm--accept" type="button" aria-label="Accept cookie Ploicy, close dialog" title="Accept and close">&#10003; <?php echo $gdprAcceptText; ?></button>
+			<button class="cta cta--md btn btn--accept gtm--accept" type="button" aria-label="Accept cookie Ploicy, close dialog" title="Accept and close">&#10003; <?php echo $gdprAcceptText; ?></button>
 			<?php endif;?>
+
+			<a href="<?php echo get_page_link(256); ?>">Learn More</a>
 
 			<?php if( $gdprDeclinetext ): ?>
 			<button class="btn btn--decline gtm--decline" type="button" aria-label="Decline cookie Ploicy, close dialog" title="Decline and close">&#10005; <?php echo $gdprDeclinetext; ?></button>
