c843e08a6f1a961e5a77db702468e49692245b73
Stephen Greig <stephengreig@Stephens-MacBook-Pro.local>
2019-08-22 23:56:49 +0100

    Added content animations on Copy & Imae Boxed modules ('checkerboards')

--- wp-content/themes/livestyled/templates/pb-copy-image-boxed--copy.php
+++ wp-content/themes/livestyled/templates/pb-copy-image-boxed--copy.php
@@ -3,18 +3,18 @@
     <div class="box-sub-container">
 
         <?php if( $sectionHeaderIcon ) : ?>
-        <img class="section__header-icon lazyload anim-500 anim-d-700" data-animate="fadeInUp" data-src="<?php echo $sectionHeaderIcon['url']; ?>" alt="<?php echo $sectionHeader; ?>">
+        <img class="section__header-icon lazyload anim-500 anim-d-1000" data-animate="fadeInUp" data-src="<?php echo $sectionHeaderIcon['url']; ?>" alt="<?php echo $sectionHeader; ?>">
         <?php endif; ?>
 
         <?php if( $sectionHeader ) : ?>
-        <h1 class="section__header section__header--copy-image anim-500 anim-d-800" data-animate="fadeInUp"><?php echo $sectionHeader; ?></h1>
+        <h1 class="section__header section__header--copy-image anim-500 anim-d-1200" data-animate="fadeInUp"><?php echo $sectionHeader; ?></h1>
         <?php endif; ?>
 
         <?php if( $sectionSubHeader ) : ?>
-        <h2 class="section__sub-header section__sub-header--copy-image anim-500 anim-d-800" data-animate="fadeInUp"><?php echo $sectionSubHeader; ?></h2>
+        <h2 class="section__sub-header section__sub-header--copy-image anim-500 anim-d-1200" data-animate="fadeInUp"><?php echo $sectionSubHeader; ?></h2>
         <?php endif; ?>
 
-        <div class="section__copy anim-500 anim-d-1000" data-animate="fadeInUp">
+        <div class="section__copy anim-500 anim-d-1400" data-animate="fadeInUp">
             <?php if( $sectionCopy ) : echo $sectionCopy; endif; ?>
 
             <?php 
